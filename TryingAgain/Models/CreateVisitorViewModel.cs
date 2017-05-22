dbe1e6ffc9f57a211c033dfc5fcbf988fccc3e8c
User <User@MICHELLEW2617>
2017-05-22 15:56:45 -0500

    init commit

---
+++ TryingAgain/Models/CreateVisitorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TryingAgain.Entities;
+
+namespace TryingAgain.Models
+{
+    public class CreateVisitorViewModel
+    {
+        public List<Visitor> Visitors { get; set; }
+
+        public CreateVisitorViewModel()
+        {
+            Visitors = new List<Visitor>();
+        }
+    }
+}
